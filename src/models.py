a4679ab46a18a6ac670ecc7e4f579fe991d6578c
hector27gab <147425846+hector27gab@users.noreply.github.com>
2024-01-29 18:40:31 +0000

    updated

--- src/models.py
+++ src/models.py
@@ -7,28 +7,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key = True)
+    image_src = Column(String(500), nullable = False)
+    likes = Column(Integer, nullable = False) 
+    description = Column(String(500), nullable = False)
+    liked = relationship("liked")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Liked(Base):
+    __tablename__ = "liked"
+    id = Column(Integer, primary_key = True)
+    number_of_likes = Column(Integer, nullable = False)
+    post_id = Column(Integer, ForeignKey("post.id"), nullable = False)
+    userlikes = relationship("userlikes")
 
-    def to_dict(self):
-        return {}
+class Userlikes(Base):
+    __tablename__ = "userlikes"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100), nullable = False)
+    favorite_posts = relationship("user")
 
-## Draw from SQLAlchemy base
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key = True)
+    email = Column(String(30), nullable = False)
+    password = Column(String(500), nullable = False)
+    userlikes = Column(Integer, ForeignKey("userlikes.id"), nullable = False)
+    user_profile = relationship("profile", backref = "user")
+
+class Profile(Base):
+    __tablename__ = "profile"
+    id = Column(Integer, primary_key = True)
+    name = Column(String(20), nullable = False)
+    description = Column(String(50), nullable = True)
+    profile_pic_src = Column(String(500), nullable = True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable = False)
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
